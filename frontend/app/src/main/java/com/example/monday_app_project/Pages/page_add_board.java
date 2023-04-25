4ab808a694b61b143b469c4bb6fd02698925587b
ptdat4823 <phamtiendat4823@gmail.com>
2023-04-25 17:26:04 +0700

    fix ui board

--- frontend/app/src/main/java/com/example/monday_app_project/Pages/page_add_board.java
+++ frontend/app/src/main/java/com/example/monday_app_project/Pages/page_add_board.java
@@ -4,24 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.example.monday_app_project.R;
+import com.example.monday_app_project.Util.SwitchActivity;
 
 public class page_add_board extends AppCompatActivity {
 
     ImageButton btnClose = null;
+
+    Button btnNewBoard = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page_add_board);
         getSupportActionBar().hide();
 
+        btnNewBoard = (Button) findViewById(R.id.btn_create_new_board);
         btnClose = (ImageButton) findViewById(R.id.btn_close);
         btnClose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 page_add_board.super.onBackPressed();
+            }
+        });
+        btnNewBoard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SwitchActivity.switchToActivity(getWindow().getContext(), page_task_board.class);
             }
         });
     }
--- frontend/app/src/main/java/com/example/monday_app_project/Pages/page_task_board.java
+++ frontend/app/src/main/java/com/example/monday_app_project/Pages/page_task_board.java
@@ -3,15 +3,35 @@ package com.example.monday_app_project.Pages;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 import com.example.monday_app_project.R;
 
 public class page_task_board extends AppCompatActivity {
 
+    ImageButton btnBack = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page_task_board);
         getSupportActionBar().hide();
+
+        btnBack = (ImageButton) findViewById(R.id.btn_back);
+        TableLayout taskboard = (TableLayout) findViewById(R.id.taskboard);
+        TableRow task1 = (TableRow) getLayoutInflater().inflate(R.layout.listview_item_custom_row, null);
+        TableRow task2 = (TableRow) getLayoutInflater().inflate(R.layout.listview_item_custom_row, null);
+        taskboard.addView(task1);
+        taskboard.addView(task2);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                page_task_board.super.onBackPressed();
+            }
+        });
+
     }
 }
